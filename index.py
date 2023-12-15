dc7424561662750f91104145a84cc7ebe62068bf
vhugds <153622281+vhugds@users.noreply.github.com>
2023-12-15 11:53:08 +0100

    Add files via upload
    practicing the video

---
+++ index.py
@@ -0,0 +1,27 @@
+print("hello world")
+height=55.78
+print(height)
+name="michael"
+print(name)
+stuff=["books","pens","cloths"]
+print(stuff)
+class_a_students=10
+print(class_a_students)
+name_of_prefect="dominion"
+print(name_of_prefect)
+my_phone="android,ios"
+print(my_phone)
+my_company_name="techcrecord"
+print(my_company_name)
+is_the_company_registerd=True
+print(is_the_company_registerd)
+info="i_am_a_programmer"
+print(info)
+music="lyrics"
+print(music)
+lyrics="he_lives_in_u_he_in_me_he-watches_over_every_thing_we_see_into_the_waters_into_the_truth_in_all_complection_he_lives_in_u"
+print(lyrics)
+cartoon="maya and the bee the golden orb"
+print(cartoon)
+home_work="download steam"
+print(home_work)
